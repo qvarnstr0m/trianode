c4901d54a75cbb7c3364af8ad8a88825bc7f317e
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-08-11 22:37:59 +0200

    lagt till social media icons

--- footer.php
+++ footer.php
@@ -3,6 +3,13 @@
     <div>
         <p><a href="<?php bloginfo('stylesheet_directory'); ?>/privacy-policy/">Privacy policy</a> | &copy; Trianode Investment 2022 | King James St, London, Gb | Web: <a href="mailto:mqvarnstrom80@gmail.com">qvarnstr0m</a></p>
     </div>
+    <!-- social media icons for later use, no links -->
+    <div>
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/icons/facebook-icon.png" alt="Facebook icon" class="socialicons">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/icons/linkedin-icon.png" alt="Linkedin icon" class="socialicons">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/icons/twitter-icon.png" alt="Twitter icon" class="socialicons">
+    <img src="<?php bloginfo('stylesheet_directory'); ?>/icons/instagram-icon.png" alt="Instagram icon" class="socialicons">
+    </div>
 </footer>
 </div>
 </body>
