60b95089e66c75ab57e2613a38b06a09e9b311b8
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-08-11 21:02:30 +0200

    lagt till ticker

--- header.php
+++ header.php
@@ -21,10 +21,47 @@
                 <img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" class="logo" alt="Trianode logotype">
                 <ul>
                     <li class="page_item"><a href="<?php echo home_url(); ?>">HOME</a>
-                    <?php wp_list_pages( '&title_li=&exclude=22,20,65' ); ?>
+                    <?php wp_list_pages( '&title_li=&exclude=22,20,65,3' ); ?>
                 </ul>
             </nav>
             <div class="topimage">
                 <img src="<?php bloginfo('stylesheet_directory'); ?>/images/runnerbackground.jpg" alt="running man" class="backgroundimage" style="width:100%">
-                
             </div>
+            
+                <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+    {
+    "symbols": [
+      {
+        "proName": "FOREXCOM:SPXUSD",
+        "title": "S&P 500"
+      },
+      {
+        "proName": "FOREXCOM:NSXUSD",
+        "title": "US 100"
+      },
+      {
+        "proName": "FX_IDC:EURUSD",
+        "title": "EUR/USD"
+      },
+      {
+        "proName": "BITSTAMP:BTCUSD",
+        "title": "Bitcoin"
+      },
+      {
+        "description": "NASDAQ",
+        "proName": "SKILLING:NASDAQ"
+      }
+    ],
+    "showSymbolLogo": true,
+    "colorTheme": "light",
+    "isTransparent": true,
+    "displayMode": "adaptive",
+    "locale": "sv_SE"
+  }
+    </script>
+  </div>
+  <!-- TradingView Widget END -->
+            
--- sidebar.php
+++ sidebar.php
@@ -10,19 +10,19 @@
         </form>
     </div>
     <div id="locations">
-    <div id="sidebarsections">
+    <div class="sidebarsections">
             <p>Trianode global offices:</p>
         </div>
-        <div id="sidebarsections">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/frankfurt.jpg" id="cities" alt="Frankfurt">
+        <div class="sidebarsections">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/frankfurt.jpg" class="cities" alt="Frankfurt">
             <p><a href="mailto:frankfurt@trianodeinvestment.com">Contact Frankfurt Office</a></p>
         </div>
-        <div id="sidebarsections">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/newyork.jpg" id="cities" alt="New York">
+        <div class="sidebarsections">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/newyork.jpg" class="cities" alt="New York">
             <p><a href="mailto:newyork@trianodeinvestment.com">Contact New York Office</a></p>
         </div>
-        <div id="sidebarsections">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/london.jpg" id="cities" alt="London">
+        <div class="sidebarsections">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/london.jpg" class="cities" alt="London">
             <p><a href="mailto:london@trianodeinvestment.com">Contact London Office</a></p>
         </div>
     </div>
